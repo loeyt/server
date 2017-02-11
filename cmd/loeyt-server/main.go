0ef73de33528d5564a33bd55ff7ace379e8382bb
Luit van Drongelen <luit@luit.eu>
2017-02-11 16:03:22 +0100

    TLS config tuning and CI improvement

--- cmd/loeyt-server/main.go
+++ cmd/loeyt-server/main.go
@@ -208,6 +208,20 @@ func tlsConfig() *tls.Config {
 	}
 	return &tls.Config{
 		GetCertificate: manager.GetCertificate,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			// tls.X25519, // Go 1.8 only
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+			// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+		NextProtos: []string{"h2", "http/1.1"},
 	}
 }
 
