3dc7b18b5ec998a91efa66c2e8236fd1d118cb2c
Luit van Drongelen <luit@luit.eu>
2017-02-11 17:04:23 +0100

    use Go 1.8 TLS features

--- cmd/loeyt-server/main.go
+++ cmd/loeyt-server/main.go
@@ -210,14 +210,14 @@ func tlsConfig() *tls.Config {
 		GetCertificate: manager.GetCertificate,
 		CurvePreferences: []tls.CurveID{
 			tls.CurveP256,
-			// tls.X25519, // Go 1.8 only
+			tls.X25519,
 		},
 		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		},
